3703b8e3949a3b9c7a144f20d59998a596580912
张为杰 <zhangwj0520@hotmail.com>
2020-04-24 08:06:19 +0800

    refactor(routes): hashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Switch, BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { Switch, HashRouter as Router, Route, Redirect } from 'react-router-dom'
+// import { Switch, BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import App from '@layouts/index'
 import { useStore } from '@hooks/useStore'
 
